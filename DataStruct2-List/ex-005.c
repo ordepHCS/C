5604f25cc6a4c99fef637be24d04f3d5378c2040
Pedro <peecampossobrinho@gmail.com>
2024-11-30 21:55:00 -0400

    C

---
+++ DataStruct2-List/ex-005.c
@@ -0,0 +1,34 @@
+/*Mostre as cinco primeiras trocas que os algoritmos de ordenação a seguir realizam para ordenar a lista [5, 1, 3, 2, 6, 4] 
+em ordem crescente. 
+
+a) Método da bolha (bubble sort) 
+    
+    i =    0  1  2  3  4  5   
+R:        [5, 1, 3, 2, 6, 4]
+    1 =   [1, 3, 2, 4, 6, 5]
+    2 =   [1, 2, 3, 4, 6, 5]
+    3 =   [1, 2, 3, 4, 6, 5]
+    4 =   [1, 2, 3, 4, 5, 6]
+    5 =   [1, 2, 3, 4, 5, 6] array ordenado :D
+
+b) Método da inserção (insertion sort) 
+
+    i =    0  1  2  3  4  5
+R:        [5, 1, 3, 2, 6, 4]
+    1 =   [1, 5, 3, 2, 6, 4]
+    2 =   [1, 3, 5, 2, 6, 4]
+    3 =   [1, 3, 2, 5, 6, 4]
+    4 =   [1, 2, 3, 5, 6, 4]
+    5 =   [1, 2, 3, 4, 5, 6] array ordenado :D
+
+
+c) Método da seleção (selection sort) 
+
+    i =    0  1  2  3  4  5
+R:        [5, 1, 3, 2, 6, 4]
+    1 =   [1, 5, 3, 2, 6, 4]
+    2 =   [1, 2, 3, 5, 6, 4]
+    3 =   [1, 2, 3, 5, 6, 4]
+    4 =   [1, 2, 3, 4, 6, 5]
+    5 =   [1, 2, 3, 4, 5, 6] array ordenado :D
+ */
