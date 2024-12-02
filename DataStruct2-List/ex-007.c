603f773a04996bb95660c5de161561507dfea146
Pedro <peecampossobrinho@gmail.com>
2024-12-01 20:40:26 -0400

    C

---
+++ DataStruct2-List/ex-007.c
@@ -0,0 +1,15 @@
+/* 7 - Discorra sobre o uso das seguintes estruturas para implementação de 
+   pilhas e filas. Explique as limitações, as vantagens e as restrições envolvidas. 
+
+a) Lista linear com vetor 
+
+   R =
+
+b) Lista simplesmente encadeadas 
+
+   R =
+
+c) Lista duplamente encadeada 
+
+   R = 
+*/
