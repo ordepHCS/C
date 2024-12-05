adf3202e45c4b0cd7d215ed6301e79122821db17
Pedro <peecampossobrinho@gmail.com>
2024-12-04 22:27:35 -0400

    C

--- DataStruct2-List/ex-018.c
+++ DataStruct2-List/ex-018.c
@@ -6,4 +6,15 @@ Insertion sort = melhor = O(n) - pior = O(n^2)
 Quick sort = melhor = O(n log n) - pior = O(n^2)
 Selection sort = melhor = O(n^2) - pior = O(n^2)
 Heap sort = melhor = O(n log n) - pior = O(n log n)
+
+para essa questao foi usado = chatgpt e esses video:
+https://youtu.be/k2XkYEUH9nA?si=7nfVZCVnTbRQ6EDc
+https://youtu.be/tR6S4dz6UGA?si=QBFUehB7uZaHOiPW
+
+sandro curio mestre sempre me guiando desde o ensino medio(eu esqueci como se calcula logaritmo ._.)
+
+e isso foi uma otima lista muito provavelmente irei refazer algumas questões futuramente provavelmente nao em C mas enfim
+espero que não tenha faltado nenhum detalhe e boa sorte pra mim na prova :D
+
+as referencias que usei para esta lista estao dentro dos arquivos em comentarios 
 */
