0f778de1d486d2f69601db2e1cc992f553c2b433
Pedro <peecampossobrinho@gmail.com>
2024-12-04 22:10:53 -0400

    C

---
+++ DataStruct2-List/ex-017.c
@@ -0,0 +1,18 @@
+/* 17 - Analise as asserções e a relação proposta entre elas. 
+
+1 - A matriz de adjacência utiliza uma matriz N x N para armazenar o grafo, onde N é o número de vértices. Tem alto custo computacional, O(n²). 
+Uma aresta é representada por uma marca na posição (i, j) da matriz, isto é, a aresta liga o vértice i ao j.
+
+2 - A figura abaixo ilustra corretamente a utilização de uma matriz de adjacências para representar um grafo e um dígrafo. 
+Estes se diferem porque o último é direcionado enquanto o primeiro, não. 
+
+a figura esta no slide :p
+
+A - As asserções I e II são proposições verdadeiras, e a II é um exemplo correto da I. 
+
+B - As asserções I e II são proposições verdadeiras, mas a II não é um exemplo correto da I. 
+
+C - A asserção I é uma proposição verdadeira, mas a II é uma proposição falsa. 
+
+  R = A.
+*/
