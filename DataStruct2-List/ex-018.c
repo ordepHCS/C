2f92b9dd80a03121a9e61fd26a54659ffd7fe064
Pedro <peecampossobrinho@gmail.com>
2024-12-04 22:19:38 -0400

    C

---
+++ DataStruct2-List/ex-018.c
@@ -0,0 +1,9 @@
+/* Complete o quadro abaixo com as complexidades assintóticas dos algoritmos de ordenação. Use a notação O. 
+
+Bubble sort = melhor = O(n) - pior = O(n^2)
+Merge sort = melhor = O(n log n) - pior = O(n log n)
+Insertion sort = melhor = O(n) - pior = O(n^2)
+Quick sort = melhor = O(n log n) - pior = O(n^2)
+Selection sort = melhor = O(n^2) - pior = O(n^2)
+Heap sort = melhor = O(n log n) - pior = O(n log n)
+*/
